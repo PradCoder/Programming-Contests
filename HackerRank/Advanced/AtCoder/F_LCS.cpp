bfa79539aaef86f269bcddf0e6ba2fdbe487e5d7
PradCoder <2010pesara@gmail.com>
2021-06-20 15:05:00 -0400

    Added LCS solution from Errichto

--- HackerRank/Advanced/AtCoder/F_LCS.cpp
+++ HackerRank/Advanced/AtCoder/F_LCS.cpp
@@ -18,9 +18,9 @@ typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int,int> pi;
 
-const ll INF = 1e18;
-const int32_t M = 1e9+7;
-const int32_t MM=998244353;
+const ll INF = 1e9+5;
+//const int32_t M = 1e9+7;
+//const int32_t MM=998244353;
 
 /*
     a x y b
@@ -30,20 +30,57 @@ y   1 1 2 2
 x   1 2 2 3
 b   1 2 2 3
 
-
-
 */
-string solve(string s, string t){
-    vector<pi> dp = vector<pi>(s.length()*t.length());
 
-    return s
+template <typename T> void max_self(T& a, T b) {
+    a = max(a,b);
+}
+
+bool first_row_or_column(pair<int,int> p){
+    return p.first == 0 || p.second == 0;
+}
+
+string solve(string s, string t){
+    int n = s.length();
+    int m = t.length();
+    
+    vector<vector<pair<int, pair<int,int>>>> dp(n+1, vector<pair<int,pair<int,int>>>(m+1, {-INF,{0,0}}));
+
+    dp[0][0] = {0, {0,0}};
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(s[i] == t[j]){
+                max_self(dp[i+1][j+1], make_pair(dp[i][j].first + 1, make_pair(i,j)));                         
+            }
+            max_self(dp[i+1][j], make_pair(dp[i][j].first, make_pair(i,j)));
+            max_self(dp[i][j+1], make_pair(dp[i][j].first, make_pair(i,j)));
+        }
+    }
+    pair<int,pair<int,int>> answer = {0,{0,0}};
+    for(int i = 0; i <= n; i++){
+        for(int j = 0;j <= m; j++){
+            max_self(answer, make_pair(dp[i][j].first, make_pair(i,j)));
+        }
+    }
+    string ans;
+    pair<int,int> cur = answer.second;
+    while(!first_row_or_column(cur)){
+        int i = cur.first;
+        int j = cur.second;
+        pair<int,int> previous = dp[i][j].second;
+        if(previous == make_pair(i-1,j-1)){
+            ans+= s[i-1];
+        }
+        cur = previous;
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
 }
 
 int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int a,b;
 	string s,t;
 	cin >> s >> t;
 	cout << solve(s,t)<< "\n";
