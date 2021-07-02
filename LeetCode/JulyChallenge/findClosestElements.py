2826c4d164f234c6d336ad8e66a1f223ab239624
PradCoder <2010pesara@gmail.com>
2021-07-02 14:21:50 -0400

    Day 2 of the July challenge

---
+++ LeetCode/JulyChallenge/findClosestElements.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        arr.sort(key=(lambda z : abs(z-x)))
+        lst = arr[:k]
+        lst.sort()
+        return lst
