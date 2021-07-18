3a16f613ca0f5d33a0e7bf1022e742d3af04256e
PradCoder <2010pesara@gmail.com>
2021-07-18 15:24:24 -0400

    Added a cleaner version of sparse table, compiles but gives segfault

--- HackerRank/Advanced/rangeMinimumQuery.cpp
+++ HackerRank/Advanced/rangeMinimumQuery.cpp
@@ -5,6 +5,7 @@
  * */
 #include "bits/stdc++.h"
 #include <cstdint>
+#include <utility>
 
 #define F                   first
 #define S                   second
@@ -26,9 +27,10 @@ typedef pair<int,int> pi;
 const ll INF = 1e18;
 const int32_t M = 1e9+7;
 const int32_t MM=998244353;
+#define MAXN  (int)1e5
 
-int solve(int st[][],int log[], int l, int r){
-    int j = log[r - l +1];
+int solve(int st[MAXN][MAXN], int l, int r){
+    int j = (int) log2(r - l +1);
     int minimum = min(st[l][j], st[r - (1 << j) + 1][j]);
     return minimum;
 }
@@ -36,33 +38,28 @@ int solve(int st[][],int log[], int l, int r){
 int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-/** Range sum query
-    int st[MAXN][K + 1];
-    for (int i = 0; i < N; i++){
-        st[i][0] = f(array[i]);
+// Range sum query
+    int st[MAXN][MAXN];
+    vector<pi> v = vector<pi>();
+    int n,q;
+    cin >> n >> q;
+     
+    for (int i = 0; i < n; i++){
+        cin >> st[i][0];
+    }
+    for (int i = 0; i< q;i++){
+        int a,b;
+        cin >> a >> b;
+        v.push_back(make_pair(a,b));
     }
 
-    for(int j = 1; j <= K; j++){
-        for(int i = 0; i + (1 << j) <= N; i++){
-            st[i][j] = f(st[i][j-1], st [i + (1 << (j -1))][j - 1]);
+    for(int j = 1; (1 << j) <= n; j++){
+        for(int i = 0; i + (1 << j) < n; i++){
+            st[i][j] = min(st[i][j-1], st[i + (1 << (j -1))][j - 1]);
         }
     }
-    **/
-    int log[M];
-    log[1] = 0;
-    for (int i = 2; i <= M; i++){
-        log[i] = log[i/2] + 1;
+    
+    for(int j = 0;j<q;j++){
+        cout << solve(st,v[j].first,v[j].second) << "\n";
     }
-
-    K = math.log(K);
-    int st[M][K + 1];
-
-    for (int j = 1; j <= K; j++){
-        for(int i = 0; i + (1 << j) <= N; i++){
-            st[i][j] = min(st[i][j-1], st[i + (1 << (j - 1))][j - 1]);
-        }
-    }
-
-	cin >> a >> b >> x;
-	cout << a << " " << b << " " << x << "\n";
 }
