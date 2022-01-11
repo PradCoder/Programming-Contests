ea35655b17ef3138bd6a0473717ac781e6f56df6
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-01-10 23:59:24 -0500

    Day 10

--- USACO/transform.cpp
+++ USACO/transform.cpp
@@ -9,8 +9,43 @@ LANG: C++
 
 using namespace std;
 
-int solve(int n, vector<string> v){
-    return 0;
+/*
+#1: 90 Degree Rotation: The pattern was rotated clockwise 90 degrees.
+#2: 180 Degree Rotation: The pattern was rotated clockwise 180 degrees.
+#3: 270 Degree Rotation: The pattern was rotated clockwise 270 degrees.
+#4: Reflection: The pattern was reflected horizontally (turned into a mirror 
+    image of itself by reflecting around a vertical line in the middle of the image).
+#5: Combination: The pattern was reflected horizontally and then subjected to one 
+    of the rotations (#1-#3).
+#6: No Change: The original pattern was not changed.
+#7: Invalid Transformation: The new pattern was not obtained by any of the above methods.
+*/
+
+
+int mod(int n,int j){
+    while(j < 0){
+        j += n;
+    }
+    return (j % n);
+}
+
+int solve(int n, vector<string> va, vector<string> vb){
+    int mi = 8;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+
+            if(v[i % n][j % n] == v[i%n][j%n]){
+                mi = min(mi,1);
+            }else if (v[i%n][j%n] == v[i%n][j%n]){
+                mi = min(mi,2);  
+            }else if (v[i%n][j%n] == [i%n][j%n]dfs){
+                mi = min(mi,3); 
+            }else if (sdfsf == sdfs){
+                mi = min(mi,4); 
+            }else if (
+        }
+    }
+    return mi;
 }
 
 int main(){
@@ -33,5 +68,7 @@ int main(){
     for(int i = 0; i < n; i++){
         fin >> va[i];
     }
+
+    fout << solve(n, vb, va);
     return 0;
 }
