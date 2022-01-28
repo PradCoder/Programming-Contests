0eeec86651686cd2ff6faaebba43dbb911225821
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-01-28 17:29:45 -0500

    Added files from cses problem set

--- USACO/beads.cpp
+++ USACO/beads.cpp
@@ -13,14 +13,15 @@ White beads can be colored anything.
 */
 
 int solve1 (int n, string str){
-    int count_a_b = 0;
+    int count_w_r = 0;
     for(int i = 0; i < n; i++){
         int count_w_b = 0;
-        int count_w_a = 0;
         for(int j = 0; j < i; j++){
             if(str[i] == 'w' || str[j] == 'b'){
-                
-            }    
+               count_w_b++;
+            }else if(str[i] == 'w' || str[j] == 'r'){
+               count_w_r++;
+            } 
         }
     }
     return val;    
