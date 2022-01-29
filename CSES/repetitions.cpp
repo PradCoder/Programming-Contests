e31283cbe2377ef5812332c92525bf66e97afe50
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-01-28 22:42:20 -0500

    Repetitions added

---
+++ CSES/repetitions.cpp
@@ -0,0 +1,30 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main(){
+    int n;
+    string str;
+    cin >> str;
+    n = str.length();
+    int count = 1;
+    int m = 0;
+    int i = 0;
+    if(n > 1 && str[i] == str[i+1]){
+        count = 2;
+    }else{
+        count = 1; 
+    }
+    m = count;
+    for(i = 2; i < n-1; i++){
+        if(str[i] == str[i-1]){
+            count++;
+        }else{
+            m = max(m,count); 
+            count = 1;
+        }
+    }
+    m = max(m,count); 
+    cout << m;
+    return 0;
+}
