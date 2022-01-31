6cc2e5a53b20254108ec3be45f0858d6f6dbda77
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-01-30 23:26:48 -0500

    Added Increasing Array

--- CSES/increasingArr.cpp
+++ CSES/increasingArr.cpp
@@ -1,17 +1,30 @@
 #include "bits/stdc++.h"
-#include <vector>
 
 using namespace std;
 
-int solve(vector<int> v){
-    return 0;
+long int solve(int n, vector<long int> vec){
+    if(n == 1){
+        return 0;
+    }
+    long int m = 0;
+    for(int i = 1; i < n; i++){
+       if(vec[i-1] > vec[i]){
+           m += vec[i-1] - vec[i];
+           vec[i] += vec[i-1] - vec[i];
+
+       } 
+    }
+    return m;
 }
 
 int main(){
     int n;
-    vector<int> vec;
+    vector<long int> vec;
     cin >> n;
-    vec = vector<int>(n,0);
-    cout << solve(vec);
+    vec = vector<long int>(n,0);
+    for(int i = 0; i < n; i++){
+        cin >> vec[i];
+    }
+    cout << solve(n,vec);
     return 0;
 }
