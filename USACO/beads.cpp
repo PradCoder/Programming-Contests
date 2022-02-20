b1066e4121aa2654ec96995f285cc2731928a133
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-02-20 15:18:59 -0500

    Added consecutive accummulate draft

--- USACO/beads.cpp
+++ USACO/beads.cpp
@@ -19,17 +19,30 @@ wwwbbrwrbrbrrbrbrwrwwrbwrwrrb
 int solve1 (int n, string str){
    int valr = 0;
    int cr = 0;
-   for(int i = 0; i<n;  i++){
-       //longest consecutive chain 
+   int valb = 0;
+   int cb = 0;
+   int cw = 0;
+   for(int i = 0; i < n;  i++){
+       //longest consecutive chain b and r
        cr = 0;
+       cb = 0;
+       cw = 0;
        for(int j = i; j < n;j++){
-            if(str[j] == "r"){
-                cr++;
+            if(str[j] == 'b' || str[j] == 'w'){
+                cb++;
             }else{
-                break
+                break;
             }
        }
-        valr = max(valr,cr)
+       for(int j = i; j < n;j++){
+            if(str[j] == 'r' || str[j] == 'w'){
+                cr++;
+            }else{
+                break;
+            }
+       }
+        valr = max(valr,cr);
+        valb = max(valb,cb);
    } 
    return valr;    
 }
