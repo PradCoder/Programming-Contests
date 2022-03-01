89e63c438113f7f5c97fdd02ef7f7616711b2f00
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-02-28 23:47:43 -0500

    Added B from solutions list

--- Codeforces#773/B.cpp
+++ Codeforces#773/B.cpp
@@ -29,22 +29,31 @@ typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int,int> pi;
 
-string solve(vector<int> &v){
-    
-    return "";
-}
-
 int main(){
-    int n;
-    cin >> n;
-    for(int i = 0; i < n; i++){
-        int m;
-        cin >> m;
-        vector<int> v(m);
-        while(m--){
-            cin >> v[m];
+    if (1) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+    }
+    int t;
+    cin >> t;
+    while (t--){
+        int n;
+        cin >> n;
+        map <int, int> d;
+        for (int i = 0; i < n; i++){
+            int x;
+            cin >> x;
+            d[x]++;
         }
-        cout << solve(v) << "\n";
+        int cnt = 0;
+        for (auto i : d){
+            ++cnt;
+        }
+        int cur_cnt = cnt;
+        for (int k = 1; k <= n; k++){
+            cout << max(k,cnt) << "\n";
+        }
     }
     return 0;
 }
