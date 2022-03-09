386dde7f3b5df138d2943cbd4076063e7f67cdc5
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-03-08 23:58:52 -0500

    Added stuff

--- Codeforces#773/myC.cpp
+++ Codeforces#773/myC.cpp
@@ -1,13 +1,15 @@
 #include "bits/stdc++.h"
-
+#include <cstdint>
 using namespace std;
 
 /*
  * 
  *
  * */
-int solve(int x, vector<int> v){
-    vector<int> inVec;
+int solve(int64_t x, vector<int64_t> aux){
+    sort(aux.begin(),aux.end());
+    vector<bool> vbol;
+    int i = 0, j = 0, ans = 0;
     for(int i = 0; i < v.size(); i++){
         if( v[i]*x == v[i] ){
         
@@ -20,13 +22,14 @@ int main(){
     int t;
     cin >> t;
     while(t--){
-        int n,x;
+        int n;
+        int64_t x;
         cin >> n >> x;
-        vector<int> v;
-        for(int i = 0; i < n; i++){
-            cin >> v[i]; 
+        vector<int64_t> aux(n);
+        for(auto& it : aux){
+            cin >> it;
         }
-        cout << solve(x,v) << "\n";
+        cout << solve(x,aux) << "\n";
     }
     return 0;
 }
