b9a19bca1e51da2ee486e6bc9ae8ff35797383f1
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-03-12 21:38:54 -0500

    started a small version of D

---
+++ myD.cpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+#include <cstdint>
+using namespace std;
+
+/*
+ * 
+ *
+ * */
+int solve(vector<int64_t> aux){
+    sort(aux.begin(),aux.end());
+    vector<bool> vbol;
+    int i = 0, j = 0, ans = 0;
+    for(int i = 0; i < v.size(); i++){
+        if( v[i]*x == v[i] ){
+        
+        } 
+    }
+    return inVec;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n
+        vector<int64_t> aux(n);
+        for(auto& it : aux){
+            cin >> it;
+        }
+        cout << solve(aux) << "\n";
+    }
+    return 0;
+}
