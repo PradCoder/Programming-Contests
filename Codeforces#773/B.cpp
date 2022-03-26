5af5db3e286db2d1f1d91c9107e4ad5450a8269b
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-03-25 23:38:34 -0400

    adjusted question A again it produced some right answers

--- Codeforces#773/B.cpp
+++ Codeforces#773/B.cpp
@@ -36,20 +36,6 @@ int main(){
     while (t--){
         int n;
         cin >> n;
-        map <int, int> d;
-        for (int i = 0; i < n; i++){
-            int x;
-            cin >> x;
-            d[x]++;
-        }
-        int cnt = 0;
-        for (auto i : d){
-            ++cnt;
-        }
-        int cur_cnt = cnt;
-        for (int k = 1; k <= n; k++){
-            cout << max(k,cnt) << "\n";
-        }
     }
     return 0;
 }
