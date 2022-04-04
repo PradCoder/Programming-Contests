058765724da1d33abaaab860063345a00b835920
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-04-03 23:39:16 -0400

    a2Oj is fine but isn't fully correct

--- A2OJ/BeautMat.cpp
+++ A2OJ/BeautMat.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(){
 	int n = 5;
     int i,j,s1,s2;
-    vector<vector<int>> v;
+    vector<vector<int>> v = vector<vector<int>>(n, vector<int>(n,0));
     i = j = s1 = s2 = 0;
     for(; i< n;i++){
         for(; j<n;j++){
@@ -22,8 +22,8 @@ int main(){
     cout << s1 << " " << s2<< endl;
     cout << abs(s1-2)+abs(s2-2) << endl;
     cout << "\n\n";
-    for(int i = 0; i < n;i++){
-        for(int j = 0; j < n; j++){
+    for(i = 0; i < n;i++){
+        for(j = 0; j < n; j++){
             cout << v[i][j] << " ";
         }
         cout << "\n";
