7047af1d7ee1323dbfec0326bd3baa2db3eeffe8
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-04-07 23:47:30 -0400

    added Present form Lena with another addition

--- A2OJ/PresentFromLena.cpp
+++ A2OJ/PresentFromLena.cpp
@@ -10,26 +10,29 @@ int main(){
     int amt = 2*n+1;
     //I've definitely lost something important, speed and creativity
     //are essential for this task.
-    for(int i = 0; i < amt/2+1; i++){
-        for(int j = amt/2+1; j >= i; j++){
+    for(int k = 0; k < amt; k++){
+        for(int l = 0; l < amt; l++){
             printf(" ");
         }
-        for(int j = 0; j < i+1; j++){
-            printf("%d",j);
+         for(int i = 0; i < amt/2+1; i++){
+             for(int j = 0; j < i+1; j++){
+                 printf("%d",j);
+             }
+             for(int j = i-1; j >= 0; j--){
+                 printf("%d",j);
+             }
+             printf("\n");
+         }
+         for(int i = amt/2-1; i >= 0; i--){
+             for(int j = 0; j < i+1; j++){
+                 printf("%d",j);
+             }
+             for(int j = i-1; j >= 0; j--){
+                 printf("%d",j);
+             }
+             printf("\n");
+         }
         }
-        for(int j = i-1; j >= 0; j--){
-            printf("%d",j);
-        }
-        printf("\n");
-    }
-    for(int i = amt/2-1; i >= 0; i--){
-        for(int j = 0; j < i+1; j++){
-            printf("%d",j);
-        }
-        for(int j = i-1; j >= 0; j--){
-            printf("%d",j);
-        }
-        printf("\n");
-    }
+    
 	return 0;	
 }
