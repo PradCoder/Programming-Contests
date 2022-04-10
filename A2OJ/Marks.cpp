d9e7bc09c915780ef5d2cbe9024595e447cbc3f8
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-04-09 23:04:47 -0400

    added Marks

--- A2OJ/Marks.cpp
+++ A2OJ/Marks.cpp
@@ -7,16 +7,18 @@ using namespace std;
 string modStr(int b){
     string s = "";
     while(b--){
-        s +="%d"
+        s +="%d";
     }
     return s;
 }
+
 int main(){
 	int a,b;
+    string s;
     cin >> a >> b;
     for(int i = 0; i < a; i++){
-        string s;
         cin >> s;
+        char *token = strtok(s,"");
         vector<int> v;
         //This is kinda iffy
         sscanf(s.c_str(), modStr(b),v);
