853265c7b5a66210ce0717f2cf90c86ca96767b7
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-04-13 23:17:05 -0400

    DZY loves sequences

---
+++ A2OJ/DZYLovesseq.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> v = vector<int> v(n,0);
+    while(n--){
+        cin >> v[i];
+    }
+    for(int i = 0; i < v.size(); i++){
+    
+    }
+	return 0;	
+}
