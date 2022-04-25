1ed0445488468af1d0e510b1665454b35f9a91b4
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-04-24 23:39:23 -0400

    Added lvl 3 DreamoonAndWifi to test lvl comprehension

---
+++ DreamoonAndWiFi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    string s1,s2;
+    cin >> s1;
+    cin >> s2;
+    float p = 0;
+    for(int i = 0; i < s1.length; i++){
+        p = 1;
+    }
+	return 0;	
+}
