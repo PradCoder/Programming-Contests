c74d054cbe5bece7c94f3dd111fbe81bf2a6a1d5
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-05-23 10:26:29 -0400

    Recursing on name that number to create all values

--- USACO/namenum.py
+++ USACO/namenum.py
@@ -1,21 +1,34 @@
+def subrecurse(i,n,formLST):
+    if n==0:
+        return ''
+    else:
+        for j in range(3):
+            return (formLST[i][j]+subrecurse(i+1,n-1,formLST))
+
+def recurse(i,n,formLST):
+    lst = []
+    for j in range(3):
+        lst.append(formLST[0][j]+recurse(1,n-1,formLST))
+    return lst
+
 def solve(dial, numstr):
     formLST = []
     for ch in numstr:
-        for i,j in dial.items():    
+        for i,j in dial.items():
             if ch == i:
                 formLST.append(j)
-
-    for i in range(len(numstr)):
+    newlst = recurse(0,len(numstr),formLST)
 
 def main():
     dial = {}
     f = open("dict.txt","r")
 
     lst = f.read().strip().split('\n')
-    print(lst) 
+    #print(lst) 
     f.close()
     f = open("namenum.in", "r")
     numstr = f.read().strip()
+    print(numstr)
     f.close()
 
     dial['2'] = "ABC"
