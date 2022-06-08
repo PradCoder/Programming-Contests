a9493a09d0d5b7e1ec96eb59e81978f70f0f81c2
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-06-08 09:45:17 -0400

    started barn repair

---
+++ USACO/barn1.py
@@ -0,0 +1,20 @@
+'''
+ID: 2010pes1
+TASK: namenum
+LANG: PYTHON3
+'''
+def solve(m,lst):
+    for i in range(m):
+def main():
+    f = open("barn1.in","r")
+    (m,s,c) = map(int, f.readline().strip().split(" "))
+    lst = []
+    for i in range(c):
+        lst(int(f.readline.strip()))
+    f.close()
+    ans = solve(m,lst)
+    f = open("barn1.out","w")
+    f.write(f"{ans}\n")
+
+if __name__="__main__":
+    main()
