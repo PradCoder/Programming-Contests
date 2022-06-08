cff119fb0e449a146c9460739417ce2d31254943
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-06-08 09:49:15 -0400

    started barn repair

--- USACO/barn1.py
+++ USACO/barn1.py
@@ -5,16 +5,18 @@ LANG: PYTHON3
 '''
 def solve(m,lst):
     for i in range(m):
+        print(lst[i])
+    return 0
 def main():
     f = open("barn1.in","r")
     (m,s,c) = map(int, f.readline().strip().split(" "))
     lst = []
     for i in range(c):
-        lst(int(f.readline.strip()))
+        lst.append(int(f.readline().strip()))
     f.close()
     ans = solve(m,lst)
     f = open("barn1.out","w")
     f.write(f"{ans}\n")
 
-if __name__="__main__":
+if __name__=="__main__":
     main()
