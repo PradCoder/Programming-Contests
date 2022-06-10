a95fcd312e4c65a896c7c59cf4c005cb3b84d55c
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-06-09 23:58:31 -0400

    can't get much beyond barn repair

--- USACO/barn1.py
+++ USACO/barn1.py
@@ -4,9 +4,15 @@ TASK: namenum
 LANG: PYTHON3
 '''
 def solve(m,lst):
+    sz = s//m 
+    fsz = s-m*sz
+    while (not folded):
+        if condition:
+            folded = True
     for i in range(m):
-        print(lst[i])
+        print((lst[i]))
     return 0
+
 def main():
     f = open("barn1.in","r")
     (m,s,c) = map(int, f.readline().strip().split(" "))
@@ -14,7 +20,7 @@ def main():
     for i in range(c):
         lst.append(int(f.readline().strip()))
     f.close()
-    ans = solve(m,lst)
+    ans = solve(m,s,lst)
     f = open("barn1.out","w")
     f.write(f"{ans}\n")
 
