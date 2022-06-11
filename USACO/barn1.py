9d87997d0a9390c9a163f8f193048f0c767c9627
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-06-10 21:46:04 -0400

    tried to figure out algorithm for barn1 with skipping

--- USACO/barn1.py
+++ USACO/barn1.py
@@ -3,15 +3,18 @@ ID: 2010pes1
 TASK: namenum
 LANG: PYTHON3
 '''
-def solve(m,lst):
-    sz = s//m 
-    fsz = s-m*sz
-    while (not folded):
-        if condition:
-            folded = True
-    for i in range(m):
-        print((lst[i]))
-    return 0
+def solve(m,s,c,lst):
+    star = lst[0]
+    fin = lst[-1]
+    count = 0
+    countalt = 0
+    for i in range(star,fin+1):
+        if(i != lst[count]):
+            countalt += 1
+        else:
+            count+=1
+    #think about taking out the bigger spaces
+    return countalt
 
 def main():
     f = open("barn1.in","r")
@@ -20,7 +23,7 @@ def main():
     for i in range(c):
         lst.append(int(f.readline().strip()))
     f.close()
-    ans = solve(m,s,lst)
+    ans = solve(m,s,c,lst)
     f = open("barn1.out","w")
     f.write(f"{ans}\n")
 
