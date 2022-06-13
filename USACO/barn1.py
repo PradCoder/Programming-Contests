c0618271798f85781db59f34aa62457e85134154
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-06-12 23:40:44 -0400

    barn1 not all cases passing, very common error of mine

--- USACO/barn1.py
+++ USACO/barn1.py
@@ -1,9 +1,29 @@
 '''
 ID: 2010pes1
-TASK: namenum
+TASK: barn1
 LANG: PYTHON3
+
+Failing case:
+50 200 10
+18
+69
+195
+38
+73
+28
+6
+172
+53
+99
+
+Expected Ans:
+10
+
+Given ans:
+190
 '''
 def solve(m,s,c,lst):
+    lst.sort()
     star = lst[0]
     fin = lst[-1]
     count = 0
@@ -12,15 +32,20 @@ def solve(m,s,c,lst):
     i = star
     while i < fin+1:
         countalt = 0
-        while (i != lst[count]):
+        while (i < lst[count]):
             countalt += 1
-            count+=1
-            i+= 1
-        outmove.append(countalt)
+            i += 1
+        if (countalt > 0):
+            outmove.append(countalt)
+        count += 1
         i+=1
-    #outmove[i-1] = outmove 
-    #think about taking out the bigger spaces
-    return countalt
+    outmove.sort()
+    ans =  c
+    if (len(outmove)>m):
+        ans += sum(outmove[:-(m-1)])
+    else:
+        ans += sum(outmove)
+    return ans
 
 def main():
     f = open("barn1.in","r")
