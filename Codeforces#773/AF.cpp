677db37b101b300ff6b1786552a7ab155241540e
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-07-03 23:58:53 -0400

    started adding AF

---
+++ Codeforces#773/AF.cpp
@@ -0,0 +1,12 @@
+#include "bits/stdc++.h"
+#include <random>
+
+using namespace std;
+
+typedef long long ll;
+typedef double ld;
+mt19937 rnd(228);
+#define TIME (clock() * 1.0 / CLOCKS_PER_SEC)
+
+const int M = 5e4 + 239;
+const int X = (int_ (
