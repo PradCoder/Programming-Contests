5140bc6e08135479ce4e18ee9d3629445a96fc56
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-07-04 23:57:48 -0400

    Added a bit to solutions

--- Codeforces#773/AF.cpp
+++ Codeforces#773/AF.cpp
@@ -9,4 +9,5 @@ mt19937 rnd(228);
 #define TIME (clock() * 1.0 / CLOCKS_PER_SEC)
 
 const int M = 5e4 + 239;
-const int X = (int_ (
+const int X = (int_ (int) (1e8) + 239;
+const int T = (1<< 17) + 239;
