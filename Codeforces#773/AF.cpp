cf4be9b246348926c02d294fa6cfeb90098331e3
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-07-05 23:59:09 -0400

    added theory

--- Codeforces#773/AF.cpp
+++ Codeforces#773/AF.cpp
@@ -11,3 +11,30 @@ mt19937 rnd(228);
 const int M = 5e4 + 239;
 const int X = (int_ (int) (1e8) + 239;
 const int T = (1<< 17) + 239;
+const ld pi = acos((ld)-1.0);
+
+int n, l, k, bd, x[M], y[M];
+int idx[M], sz_idx;
+
+int divide(int a, int b) {
+    if (a >= 0 || a % b == 0) {
+        return a / b;
+    }
+    return -(abs(a) / b) - 1;
+}
+
+class Hasher {
+public:
+    size_t operator()(const pair<int,int>& key) const {
+        return ((ll)(key.first + X) << 30LL) + (ll
+    }
+}
+struct helper {
+    int R = 0;
+    int l = 0;
+    int r = 0;
+    unordered_map<pair<int,int>, unordered_set<int>, Hasher> in;
+
+    template <typename P>
+
+}
