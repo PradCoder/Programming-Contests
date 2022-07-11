65ce1240cd28df447e84c214685733b373ddf61c
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-07-10 23:11:54 -0400

    Codeforces 805

---
+++ Codeforces#774/A.cpp
@@ -0,0 +1,12 @@
+#include "bits/stdc++.h"
+
+int main(){
+    cin >> n;
+    vector<pair<int,int>> pa;
+    vector<pair<int,int>> pa = vector<pair<int,int>>(0,n);
+    for(int i = 0; i < n; i++){
+        cin >> pa[i].first >> pa[i].second;
+    }
+
+    return 0;
+}
