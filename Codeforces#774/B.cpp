b248bdb5120c742a831ceaf517f51489473c907a
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-07-15 01:42:18 -0400

    Working on solutions for Div 2 774

--- Codeforces#774/B.cpp
+++ Codeforces#774/B.cpp
@@ -2,36 +2,37 @@
 
 using namespace std;
 
-bool Sum (vector<int> ai) {
-    bool al = true;
-    //make partitions of sets and try solving those?
-    for (int i = 0; i < ai.size(); i++){
-        
-    }
-    return al;
-}
-
-bool Count (vector<int> ai) {
-    bool bl = true;
-
-    return bl;
-}
-
 int main(){
-    int t;
-    cin >> t;
-    for (int i = 0; i < t; i++){
-        int n;
-        vector<int> ai;
-        cin >> n;
-        for(int j = 0; j < n; j++){
-            int ip;
-            cin >> ip;
-            ai.push_back(ip);
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    int t; cin >> t;
+    for (int test_number = 0; test_number < t; test_number++){
+        int n; cin >> n;
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++){
+            cin >> a[i];
         }
-        string s;
-        s = (Sum(ai) && Count(ai)) ? "YES\n" :"NO\n";
+        sort(a.begin(), a.end());
+        vector<long long> prefix_sums = {0};
+        for (int i = 0; i < n; i++){
+            prefix_sums.push_back(prefix_sums.back() + a[i]);
+        }
+        vector<long long> suffix_sums = {0};
+        for (int i = n-1; i >= 0; i--){
+            suffix_sums.push_back(suffix_sums.back() + a[i]);
+        }
+        bool answer = false;
+        for (int k = 1; k <= n; k++){
+            if (2 * k + 1 <= n){
+                long long blue_sum = prefix_sums[k + 1];
+                long long red_sum = suffix_sums[k];
+                if(blue_sum < red_sum){
+                    answer = true;
+                }
+            }
+        }
+        if (answer) cout << "YES\n";
+        else cout << "NO\n";
     }
     return 0;
 }
-
