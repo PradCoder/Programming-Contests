dd1b7ca31e1904dfb3e87a7b2258e4c890982cb9
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-07-29 23:20:47 -0400

    Finished A-playoff but it was too easy

--- EduCodeforces#124/A.cpp
+++ EduCodeforces#124/A.cpp
@@ -1,20 +1,14 @@
 #include "bits/stdc++.h"
 
+#define forn(i,n) for (int i = 0; i < n; i++)
 using namespace std;
 
-#define forn(i,n) for (int i = 0; i < int(n); i++)
-
 int main(){
-    int t;
-    cin >> t;
-    forn(tt,t) {
-        string s, a;
-        cin >> s >> a;
-        bool yes = false;
-        forn(i, s.length())
-            if (s[i] == a[0] && i % 2 == 0)
-                yes = true;
-        cout << (yes ? "YES" : "NO") << "\n";
+    int t; cin >> t;
+    forn(i,t){
+        int n;
+        cin >> n;
+        cout << int(pow(2,n)-1) << "\n";
     }
     return 0;
 }
