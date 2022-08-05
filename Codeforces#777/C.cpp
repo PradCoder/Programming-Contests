ffd4845b809de64f512970b90931c163444e1120
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-08-04 23:52:19 -0400

    Started round number 777 C

--- Codeforces#777/C.cpp
+++ Codeforces#777/C.cpp
@@ -1,48 +1,46 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+ 
 using namespace std;
-using ll = long long;
-
-#define forn(i,n) for (int i = 0; i < int(n); i++)
-
-struct point{
-    int weight, position, id;  
-};
-
-void solve(){
+ 
+void solve() {
     int n, m;
     cin >> n >> m;
-    vector<point> points(m);
-
-    forn(i, m) {
-        cin >> points[i].position >> points[i].weight;
-        points[i]. id = i + 1;
+    vector<vector<int>> a(n, vector<int> (m));
+    for (int i = 0; i < n; ++i) {
+        string s;
+        cin >> s;
+        for (int j = 0; j < m; ++j)
+            a[i][j] = s[j] - '0';
     }
-
-    sort(points.begin(), points.end(), [] (point a, point b) {
-                return a.weight < b.weight;
-            });
-
-    int sum = 0;
-    forn(i, m){
-        if(i < 2 * n) sum += points[i].weight;
-        else points.pop_back();
+    vector<array<int, 4>> ans;
+    if (a[0][0] == 1) {
+        cout << -1<< '\n';
+        return;
     }
-
-    sort(points.begin(), points.end(), [] (point a, point b) {
-                return a.position < b.position;
-            });
-
-    cout << sum << endl;
-    forn(i, n){
-        cout << points[i].id << " " << points[2 * n - i - 1].id << endl;
+    
+    for (int j = m - 1; j >= 0; --j) {
+        for (int i = n - 1; i >= 0; --i) {
+            if (a[i][j]) {
+                if (i != 0) {
+                    ans.push_back({i, j + 1, i + 1, j + 1});
+                } else {
+                    ans.push_back({i + 1, j, i + 1, j + 1});
+                }
+            }
+        }
+    }
+    cout << ans.size() << '\n';
+    for (auto i : ans) {
+        cout << i[0] << ' ' << i[1] << ' ' << i[2] << ' ' << i[3] << '\n';
     }
 }
-
-int main(){
+ 
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+ 
     int t;
     cin >> t;
-    while(t--){
+    while (t--)
         solve();
-    }
-    return 0;
 }
