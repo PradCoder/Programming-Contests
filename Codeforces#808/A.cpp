25e017838ce2bdbc12c15f4ba4ce41a37a3289e6
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-08-08 02:34:19 -0400

    Finished round 808 A

--- Codeforces#808/A.cpp
+++ Codeforces#808/A.cpp
@@ -8,11 +8,9 @@ void solve() {
     vector<int> a(n);
     for (int i = 0; i < n; ++i)
         cin >> a[i];
-    bool ok = false;
-    if(a[1] % a[0] == 0)
-        ok = true;
+    bool ok = true;
     for (int i = 1; i < n; i++){
-        if(a[i-1] > a[i] && a[i] % a[0] != 0){
+        if(a[i] % a[0] != 0){
             ok = false;
             break;
         }
