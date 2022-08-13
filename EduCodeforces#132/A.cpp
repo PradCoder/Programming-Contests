d455c5d9eafc99626c5462e533366e1f6940d9e9
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-08-12 23:02:19 -0400

    added Edu 132 Three doors

--- EduCodeforces#132/A.cpp
+++ EduCodeforces#132/A.cpp
@@ -1,14 +1,28 @@
 #include "bits/stdc++.h"
 
-#define forn(i,n) for (int i = 0; i < n; i++)
 using namespace std;
 
 int main(){
-    int t; cin >> t;
-    forn(i,t){
-        int n;
-        cin >> n;
-        cout << int(pow(2,n)-1) << "\n";
+    int tt, x, elem;
+    cin >> tt;
+    while(tt--){
+        cin >> x;
+        map <int, pair<int, bool>> dict;
+        for (int i = 0; i < 3; i++){
+            cin >> elem;
+            dict.insert({i+1, pair<int,bool>(elem, false)});
+        }
+        int count = 0;
+        for(int i = 0; i < 3; i++){
+            dict[x].second = true;
+            x = dict[x].first;
+        }
+        for(int i = 0; i < 3; i++){
+            if(dict[i+1].second){
+                count++;
+            }
+        }
+        cout << ((count == 3) ? "YES" : "NO") << "\n";
     }
     return 0;
 }
