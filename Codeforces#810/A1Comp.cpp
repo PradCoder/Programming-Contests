4b1fe6fb5e707484a4952dd135fcad4aba406a57
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-08-15 23:55:23 -0400

    started A1 solution translation 810

--- Codeforces#810/A1Comp.cpp
+++ Codeforces#810/A1Comp.cpp
@@ -1,34 +1,49 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
 #define MAXN 100010
-int x[MAXN],y[MAXN],a[MAXN],degree[MAXN];
-int n,m;
-void work(){
-	cin>>n>>m;
-	for (int i=1;i<=n;i++)
-	{
-		degree[i]=0;
+int n,m,k;
+int a[MAXN];
+void work()
+{
+	cin>>n>>m>>k;
+	for (int i=1;i<=k;i++)
 		cin>>a[i];
-	}
-	for (int i=1;i<=m;i++)
+	bool flag;
+	long long tot=0;
+	flag=0;
+	tot=0;
+	for (int i=1;i<=k;i++)
 	{
-		cin>>x[i]>>y[i];
-		degree[x[i]]++;
-		degree[y[i]]++;
+		if (a[i]/n>2)
+			flag=1;
+		if (a[i]/n>=2)
+			tot+=a[i]/n;
 	}
-	int ans=INT_MAX;
-	if (m%2==0)
-		ans=0;
-	for (int i=1;i<=n;i++)
-		if (degree[i]%2==1)
-			ans=min(ans,a[i]);
-	for (int i=1;i<=m;i++)
-		if (degree[x[i]]%2==0 && degree[y[i]]%2==0)
-			ans=min(ans,a[x[i]]+a[y[i]]);
-	cout<<ans<<endl;
+	if (tot>=m && (flag || m%2==0))
+	{
+		cout<<"Yes"<<endl;
+		return ;
+	}
+	flag=0;
+	tot=0;
+	for (int i=1;i<=k;i++)
+	{
+		if (a[i]/m>2)
+			flag=1;
+		if (a[i]/m>=2)
+			tot+=a[i]/m;
+	}
+	if (tot>=n && (flag || n%2==0))
+	{
+		cout<<"Yes"<<endl;
+		return ;
+	}
+	cout<<"No"<<endl;
+
 }
 
-int main(){
+int main()
+{
 	int casenum=1;
 	cin>>casenum;
 	for (int testcase=1;testcase<=casenum;testcase++)
