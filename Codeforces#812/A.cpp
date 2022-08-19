85d2d1954d7c4b40bbcf4b64a0555d5e50cb8107
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-08-18 23:56:53 -0400

    Started 812

---
+++ Codeforces#812/A.cpp
@@ -0,0 +1,20 @@
+#include "bits/stdc++.h"
+ 
+using namespace std;
+ 
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  int t, n; 
+  cin >> t;
+  while (t--) {
+    cin >> n >> m >> k;
+    vector<int> a(k,0);
+    for (int i = 0; i < k; i++){
+        cout << i << " ";
+    } 
+    cout << "\n";
+  }
+  
+}
