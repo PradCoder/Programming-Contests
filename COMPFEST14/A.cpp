ee3bbcf429ceb4909a824097caacea88f2fbf074
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-09-04 22:49:39 -0400

    Added First CodeFest

---
+++ COMPFEST14/A.cpp
@@ -0,0 +1,10 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    cout << (((m-1) == 0) ? ((n-1) * m) : (n * (m-1))) << "\n";
+    return 0;
+}
