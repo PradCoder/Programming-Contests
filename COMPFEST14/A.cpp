3c1b329c8e7004b756c661c685989b9ef4b5b598
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-09-05 23:42:42 -0400

    COMPFEST14 A solved

--- COMPFEST14/A.cpp
+++ COMPFEST14/A.cpp
@@ -1,9 +1,9 @@
 #include "bits/stdc++.h"
-
+ 
 using namespace std;
-
+ 
 int main(){
-    int n, m;
+    long long n, m;
     cin >> n >> m;
     cout << (((m-1) == 0) ? ((n-1) * m) : (n * (m-1))) << "\n";
     return 0;
