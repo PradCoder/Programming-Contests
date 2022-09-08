00b973694ba774d996bb0208be9db7c8f5962355
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-09-07 23:58:27 -0400

    started B on Round 19

--- Codeforces#819/B.cpp
+++ Codeforces#819/B.cpp
@@ -2,27 +2,18 @@
 
 using namespace std;
 
+int solve(){
+
+    return m;
+}
+
 int main(){
-    int t, n;
+    int a, b;
     string s1, s2;
     cin >> t;
     while(t--){
-        cin >> n >> s1 >> s2;
-        //G == B
-        bool state = true;
-        for(int i = 0; i < n ; i++){
-            if(s1[i] == s2[i]){
-               state = state && true;
-            }else{
-                if((s1[i] == 'G' || s1[i] == 'B') && (s2[i] == 'G' || s2[i] == 'B')){
-                    state = state && true;
-                }else{
-                    state = state && false;
-                    break;
-                }
-            }
-        }
-        cout << ((state) ? "YES" : "NO" ) << "\n";
+        cin >> a >> b;
+        cout << ((state) ? "YES" : solve() ) << "\n";
     }
     return 0;
 }
