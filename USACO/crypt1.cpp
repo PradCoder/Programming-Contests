510d7840288dedcb4af370861e632b0f6a0db2ce
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-09-14 21:33:44 -0400

    Added crypt1

--- USACO/crypt1.cpp
+++ USACO/crypt1.cpp
@@ -14,6 +14,14 @@ bool containment(set<int> space, string str){
     bool is_in = true;
     for (int i = 0; i < str.length(); i++){
         int a = (str[i]-'0'); 
+        if(str == "444"){
+            cout << "looped " << a << " " << space.size() << "\n";
+            for (auto i : space) {
+                cout << i << "\n"; 
+            }
+            cout << "Und stuff: " << (space.find(a) != space.end()) << "\n";
+            cout << "Ended loop\n";
+        }
         if(space.find(a) != space.end()){
             is_in = true; 
         }else{
@@ -46,7 +54,7 @@ int main(){
                 for (int l = 0; l < n; l++){
                     for (int m = 0; m < n; m++){
                         vec[(i*n*n*n*n)+(j*n*n*n)+(k*n*n)+(l*n)+m] = {v[i], v[j], v[k], v[l], v[m]};
-                        cout << (i*n*n*n*n)+(j*n*n*n)+(k*n*n)+(l*n)+m << "\n";
+                        //cout << (i*n*n*n*n)+(j*n*n*n)+(k*n*n)+(l*n)+m << "\n";
                         string a, b, c, d, e;
                         a = to_string(v[i]);
                         b = to_string(v[j]);
@@ -54,7 +62,7 @@ int main(){
                         d = to_string(v[l]);
                         e = to_string(v[m]);
                         set<int> space = {v[i], v[j], v[k], v[l], v[m]};
-                        cout << a << " " << b << " " << " " << c << " " << d << " " << e << endl;
+                        //cout << a << " " << b << " " << " " << c << " " << d << " " << e << endl;
                         int abc, de; // abc and de don't need to be checked as they are
                                      // only contain elements from the set
                         abc = stoi(a+b+c);
@@ -62,8 +70,12 @@ int main(){
 
                         string p1s, p2s; //partial products
                         p1s = to_string(abc * stoi(e)); 
-                        p2s = to_string(abc*stoi(d));
-                        cout << containment(space,(a+b+c)) << containment(space,(d+e))<< "\n"; 
+                        p2s = to_string(abc * stoi(d));
+                        if (p1s == "444" && p2s == "444"){
+                            cout << p1s << " " << p2s << "\n";
+                            cout << p2s << " " << space.size() << "\n";
+                            cout << containment(space,p1s) << " " << containment(space,p2s) << "\n";
+                        }
                         //debug containment 
                         if(containment(space, p1s) && containment(space, p2s)){
                             string str_fin;
@@ -72,7 +84,6 @@ int main(){
                                 count++;
                             }
                         }
-
                     }
                 }
             }
