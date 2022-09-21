3c4832075efa26470c7217e0a82ed294fa794600
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-09-20 22:45:30 -0400

    Started round 820

---
+++ Codeforces#820/A.cpp
@@ -0,0 +1,26 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int solve(int n, vector<int> vec){
+    int k = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = i; j < n; j++){
+            
+        }
+    }
+    return 
+}
+
+int main(){
+    int tt;
+    int n;
+    cin >> tt;
+    cin >> n;
+    vector<int> vec(n,0);
+    for (int i = 0; i < n; i++){
+        cin >> vec[i];
+    }
+    cout << solve(n, vec) << "\n";
+    return 0;
+}
