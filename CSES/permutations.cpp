daf22bb4e1dd4d9d52c6237b1301404dff6bd75f
Pesara Amarasekera <amarasep@mcmaster.ca>
2022-10-29 23:47:38 -0400

    Change for permutations

--- CSES/permutations.cpp
+++ CSES/permutations.cpp
@@ -2,52 +2,29 @@
 
 using namespace std;
 
-bool check(vector<int> arr){
-    bool sa = true;
-    int n = arr.size();
-    for(int i = 0; i < n-1; i++){
-        cout << arr[i] << " " << arr[i+1] << "\n";
-        if (abs(arr[i]-arr[i+1]) == 1){
-            sa = !sa;
-        }
-    }
-    cout << sa << "\n";
-    return sa;
-}
-
 int main(){
     int n;
     cin >> n;
     vector<int> ar(n,0);
-    for (int i = 1; i <= n; i++){
-        ar[i-1] = i; 
-        cout << i << " ";
-    }
-
-    cout << "\n";
-
+    int j = 0;
+    bool exists = true;
     for (int i = 0; i < n; i++){
-        for(int j = i; j < n; j+=2){
-           int a = ar[i%n];
-           ar[i%n] = ar[(i+1)%n];
-           ar[(i+1)%n] = a;
+        if(j>=n){
+            exists = false;
+            cout << "NO SOLUTION";
+            break;
         }
-        if(check(ar)){
-           break;         
+        ar[j] = i+1;
+        if(n&1){
+            j = (i&1) ? (j-2) : (j+3);
+        }else{
+            j = (i&1) ? (j-1) : (j+2);
         }
-        for (int i = 1; i <= n; i++){
-            ar[i-1] = i; 
-            cout << i << " ";
-        }
-        cout << "\n"; 
     }
-
-    if(check(ar)){
-        for (auto i : ar){
-            cout << i << " ";
+    if(exists){
+        for (int i = 0; i < n; i++){
+            cout << ar[i] << " ";
         }
-    }else{
-        cout << "NO SOLUTION";
     }
     cout << "\n";
     return 0;
