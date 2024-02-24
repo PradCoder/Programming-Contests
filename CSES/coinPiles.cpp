71a4a393791ba702b8c2b012d81ec9e61cbbce8a
Pesara Amarasekera <amarasep@mcmaster.ca>
2024-02-24 13:14:36 -0500

    Quadratic Time solution -not efficient

--- CSES/coinPiles.cpp
+++ CSES/coinPiles.cpp
@@ -1,36 +1,43 @@
 #include <iostream>
-
+#include "bits/stdc++.h"
 typedef unsigned int ui;
 using namespace std;
 
 //Check the algorithms here out for a bit
-string solve(ui a, ui b){
+bool solve(long a, long b){
    /* Actions: Remove 2 from L and 1 from R
     *          Remove 1 from L and 2 from R
     *
-    * Directive: Find the most efficient way of doing this 
+    * Directive: Find the most efficient way of Emptying both piles doing 1 and 2 
     *
     * */
     //simulation take 2 from max take 1 from min
-    bool state = false;
-    if((a+b) % 3 == 0){
-        if((a%2 == 1 && b%2 == 0) || (a%2 == 0 && b%2 == 1)){
-            state = true;
-        }
+
+    /*while(){
+
+        a-=1;
+        b-=2;
+    }*/
+    //Inefficient Recursion
+    if((a > 0 || b > 0) && (a >= 0 && b >= 0)){
+        return (solve(a-1,b-2) || solve(a-2,b-1));
+    }else if (a == 0 && b == 0){
+        return true;
     }
-    return (state) ? "YES" : "NO" ;
+
+    return false;
 }
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int t;
-    unsigned int a,b;
+    ui t;
+    long a,b;
     cin >> t;
     while(t--){
         cin >> a >> b;
-        cout << solve (a,b) << "\n";
+        cout << (solve (a,b) ? "YES" : "NO") << "\n";
     }
     return 0;
 }
